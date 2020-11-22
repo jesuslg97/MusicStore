c6fdc305046553308b19b5d0f55392db86592ec0
jesus <jelopezgonzalez@gmail.com>
2020-11-22 14:15:37 +0000

    Validación login y password

--- dist/js/validation-form.js
+++ dist/js/validation-form.js
@@ -1,23 +1,72 @@
-function validate_user() {
+/*Función para validar el formulario login.html*/
+function validate_login() {
+
+    //Variables
     var email = document.getElementById("email").value;
     var passwd = document.getElementById("passwd").value;
     var flag = true;
 
-    if(email.length < 2 || email.length > 20) {
-        document.getElementById("eemail").innerHTML ="Error, el email tiene que tener minimo 2 y maximo 20 letras";
+    //Validar email
+    var regex_email = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+    if(regex_email.test(email) !== true) {
+        document.getElementById("eemail").innerHTML ="El email no coincide con los estándares sobre la " +
+            "estructura de un email, por favor inténtelo de nuevo.";
         flag = false;
     } else {
         document.getElementById("eemail").innerHTML ="";
-
     }
 
-    if(passwd.length < 1 || passwd.length > 30) {
-        document.getElementById("epasswd").innerHTML ="Error, la contraseña tiene que tener minimo 1 y maximo 30 letras";
+    //Validar password
+    var regex_passwd = /^(?=.*\d)(?=.*[\u0021-\u002b\u003c-\u0040])(?=.*[A-Z])(?=.*[a-z])\S{8,16}$/;
+    if(regex_passwd.test(passwd) !== true) {
+        document.getElementById("epasswd").innerHTML ="La contraseña debe tener al entre 8 y 16 caracteres, " +
+            "al menos un dígito, al menos una minúscula, al menos una mayúscula y al menos un caracter no alfanumérico.";
         flag = false;
     } else {
         document.getElementById("epasswd").innerHTML ="";
-
     }
     return flag;
 }
 
+/*Función para validar el formulario password.html*/
+function validate_password() {
+
+    //Variables
+    var email = document.getElementById("email").value;
+    var passwd1 = document.getElementById("passwd1").value;
+    var passwd2 = document.getElementById("passwd2").value;
+    var flag = true;
+
+    //Validar email
+    var regex_email = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+    if(regex_email.test(email) !== true) {
+        document.getElementById("eemail").innerHTML ="El email no coincide con los estándares sobre la " +
+            "estructura de un email, por favor inténtelo de nuevo.";
+        flag = false;
+    } else {
+        document.getElementById("eemail").innerHTML ="";
+    }
+
+    //Validar password
+    var regex_passwd = /^(?=.*\d)(?=.*[\u0021-\u002b\u003c-\u0040])(?=.*[A-Z])(?=.*[a-z])\S{8,16}$/;
+    if(regex_passwd.test(passwd1) !== true) {
+        if(regex_passwd.test(passwd2) !== true) {
+            document.getElementById("epasswd1").innerHTML ="La contraseña debe tener al entre 8 y 16 caracteres, " +
+                "al menos un dígito, al menos una minúscula, al menos una mayúscula y al menos un caracter no alfanumérico.";
+            flag = false;
+        } else {
+            document.getElementById("epasswd1").innerHTML ="";
+        }
+    }
+
+
+    //Validar password
+    if (passwd1 !== passwd2) {
+        document.getElementById("epasswd2").innerHTML ="Las contraseñas no coinciden, por favor inténtelo de nuevo.";
+        flag = false;
+    } else {
+        document.getElementById("epasswd2").innerHTML ="";
+    }
+
+    return flag;
+}
