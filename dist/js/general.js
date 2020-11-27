ddccbb0f9258ec19a0ad033daeba9f9d6a71c781
Sara <saraalmonacid98@gmail.com>
2020-11-27 21:36:59 +0000

    Activación menú navbar según Id en product-list

---
+++ dist/js/general.js
@@ -0,0 +1,32 @@
+function activateNavbar(id) {
+    if (id==1) {
+        $('#guitarBassNav').addClass('active');
+    }
+    if (id==2) {
+        $('#guitarBassNav').addClass('active');
+    }
+    if (id==3) {
+        $('#percusionNav').addClass('active');
+    }
+    if (id==4) {
+        $('#percusionNav').addClass('active');
+    }
+    if (id==5) {
+        $('#tecladosNav').addClass('active');
+    }
+    if (id==6) {
+        $('#tecladosNav').addClass('active');
+    }
+    if (id==7) {
+        $('#iluminacionNav').addClass('active');
+    }
+    if (id==8) {
+        $('#iluminacionNav').addClass('active');
+    }
+    if (id==9) {
+        $('#djNav').addClass('active');
+    }
+    if (id==10) {
+        $('#djNav').addClass('active');
+    }
+}
